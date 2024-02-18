d54e1dfa42770234fca7869cd48a0c3fd275c958
wkrantz <krantz.w@gmail.com>
2024-02-17 17:39:30 -0800

    working nb version

--- lib/music_tools.lua
+++ lib/music_tools.lua
@@ -9,3 +9,20 @@ end
 function mode_index_to_name(index)
     return(musicutil.SCALES[index].name)
 end
+
+
+function invert_chord(notes, inversion)
+    if inversion == 0 then
+        return notes
+    elseif inversion == 1 then
+        notes[1] = notes[1] + 12
+    elseif inversion == 2 then
+        notes[1] = notes[1] + 12
+        notes[2] = notes[2] + 12
+    elseif inversion == 3 then
+        notes[1] = notes[1] + 12
+        notes[2] = notes[2] + 12
+        notes[3] = notes[3] + 12
+    end
+    return notes
+end
--- palette.lua
+++ palette.lua
@@ -6,6 +6,10 @@
 include('lib/ui')
 include('lib/music_tools')
 musicutil = require("musicutil")
+
+local nb = require "nb/lib/nb"
+
+
 mxsamples=include("mx.samples/lib/mx.samples")
 engine.name="MxSamples"
 instruments = {}
@@ -28,9 +32,10 @@ start_y=5
 
 chordname = ""
 momentary_inversion = 0
-momentary_octave = 3
+momentary_octave = 4
 notes  = {}
 chord_playing = false
+
 steps_in_scale = {0,2,4,5,7,9,11}
 
 --scale_shape = [lev1,lev2]
@@ -43,7 +48,7 @@ function add_params()
     
     params:add_number("transpose", "Key", 0, 11, 0, function(param) return transpose_string(param:get()) end)
     params:add_number('mode', 'Mode', 1, 9, 1, function(param) return mode_index_to_name(param:get()) end)
-    params:add_number('octave', 'Octave', 0, 8, 3)
+    params:add_number('octave', 'Octave', 0, 8, 4)
     params:add_number('inversion' , 'Inversion', 0, 2, 0)
     params:add_option('bass_note', 'Bass Note', {'no', 'yes'}, 2)
 
@@ -72,6 +77,11 @@ add_params()
 
 
 function init()
+    nb:init()
+    nb:add_param("voice", "voice")
+    nb:add_player_params()
+
+
     grid_dirty = false -- script initializes with no LEDs drawn
     screen_dirty = false
     momentary = {} -- meta-table to track the state of all the grid keys
@@ -94,7 +104,7 @@ end
 
 function redraw_clock() -- our grid redraw clock
     while true do -- while it's running...
-        clock.sleep(1/30) -- refresh at 30fps.
+        clock.sleep(1/10) -- refresh at 10fps.
         if grid_dirty then -- if a redraw is needed...
             grid_redraw() -- redraw...
             grid_dirty = false -- then redraw is no longer needed.
@@ -126,10 +136,16 @@ end
 
 
 function g.key(x,y,z)  -- define what happens if a grid key is pressed or released
+    if x == 16 and y == 8 and z == 1 then
+        panic()
+    end
+
+    
+    momentary[x][y] = z == 1 and (not chord_playing) or false -- if a grid key is pressed, flip it's table entry to 'on'
+    
 
   -- check the basemap to see if we are on a pressable button
     if basemap[x][y] > 0 then
-
         -- is x between 1 and 3 inclusive, aka the modifier buttons
         if x >= 1 and x <= 4 then
 
@@ -145,8 +161,6 @@ function g.key(x,y,z)  -- define what happens if a grid key is pressed or releas
                 -- momentarily change the inversion, otherwise leave it at the default set in the params
                 momentary_inversion = z == 1 and x-1 or params:get("inversion")
 
-
-
             elseif y == 5 then
 
                 if momentary[2][5] then
@@ -154,10 +168,7 @@ function g.key(x,y,z)  -- define what happens if a grid key is pressed or releas
                 end
                 
                 root = z == 1 and baseroot+(4*(x-2)) or baseroot 
-
             end
-
-
         end
 
 
@@ -175,18 +186,11 @@ function g.key(x,y,z)  -- define what happens if a grid key is pressed or releas
 
             if (chord_playing==false) then
                 play_chord(y-start_y, x - start_x + 1, z)
-                
-                -- -- tritone substitution
-                -- if y == 3 then
-                --     --play_chord(root+1, x-4, true, false,inversion,z)
-                
-                    chord_playing = z==1 and {x,y} or false
+                chord_playing = z==1 and {x,y} or false
             end
-            
         end
 
         
-        momentary[x][y] = z == 1 and true or false -- if a grid key is pressed, flip it's table entry to 'on'
 
 
   -- what ^that^ did was use an inline condition to assign our momentary state.
@@ -198,153 +202,89 @@ end
 
 
 function play_chord(row,degree,z)
-    root = momentary_octave*12 + params:get("transpose")
+    if z == 0 then
+        notes_to_engine(notes, z)
+    else
+        root = momentary_octave*12 + params:get("transpose")
 
-    -- get the notes for the base chord
+        -- get the notes for the base chord
 
-    -- main chord row
-    if row == 0 then
-        notes = musicutil.generate_chord_scale_degree(root, params:get("mode"), degree, false)
+        -- main chord row
+        if row == 0 then
+            notes = musicutil.generate_chord_scale_degree(root, params:get("mode"), degree, false)
+        end
+
+
+        -- secondary dominant row
+        if row == -1 then
+            -- the root of the secondary dominant the first note in the chord we would be playing
+            secondary_dominant_root = musicutil.generate_chord_scale_degree(root, params:get("mode"), degree, false)[1]
+
+            -- then we get the five chord in that key
+            -- play it down an octave and as a 7th by default
+            notes = musicutil.generate_chord_scale_degree(secondary_dominant_root - 12, "Major", 5, true)
+        end
+        -- seventh chord row
+        if row == 1 then
+            notes = musicutil.generate_chord_scale_degree(root, params:get("mode"), degree, true)
+        end
+
+        -- secondary mode
+        if row == 2 then
+            notes = musicutil.generate_chord_scale_degree(root, params:get("alt_mode"), degree, false)
+        end
+
+
+        -- modifications -- 
+        -- doing the bass note addition in a weird order so it's not affected by inversion, could think of a better way to do this
+        bass_note = notes[1]-12
+
+        --invert chord as specified
+        notes = invert_chord(notes, momentary_inversion)
+
+        if params:get("bass_note") == 2 then
+            notes[#notes+1] = bass_note
+        end
+
+        -- sort the notes in ascending order so that a strum will play correctly
+        table.sort(notes)
+        notes_to_engine(notes, z)
+        chordname = root + degree
     end
-
-
-    -- secondary dominant row
-    if row == -1 then
-        -- the root of the secondary dominant the first note in the chord we would be playing
-        secondary_dominant_root = musicutil.generate_chord_scale_degree(root, params:get("mode"), degree, false)[1]
-
-        -- then we get the five chord in that key
-        -- play it down an octave and as a 7th by default
-        notes = musicutil.generate_chord_scale_degree(secondary_dominant_root - 12, "Major", 5, true)
-    end
-    -- seventh chord row
-    if row == 1 then
-        notes = musicutil.generate_chord_scale_degree(root, params:get("mode"), degree, true)
-    end
-
-    -- secondary mode
-    if row == 2 then
-        notes = musicutil.generate_chord_scale_degree(root, params:get("alt_mode"), degree, false)
-    end
-
-
-    -- modifications -- 
-    -- doing the bass note addition in a weird order so it's not affected by inversion, could think of a better way to do this
-    bass_note = notes[1]-12
-
-    --invert chord as specified
-    notes = invert_chord(notes, momentary_inversion)
-
-    if params:get("bass_note") == 2 then
-        notes[#notes+1] = bass_note
-    end
-
-    -- sort the notes in ascending order so that a strum will play correctly
-    table.sort(notes)
-
-    chordname = root + degree
+    
     screen_dirty = true
 end
 
 
-function invert_chord(notes, inversion)
-    if inversion == 0 then
-        return notes
-    elseif inversion == 1 then
-        notes[1] = notes[1] + 12
-    elseif inversion == 2 then
-        notes[1] = notes[1] + 12
-        notes[2] = notes[2] + 12
-    elseif inversion == 3 then
-        notes[1] = notes[1] + 12
-        notes[2] = notes[2] + 12
-        notes[3] = notes[3] + 12
-    end
-    return notes
-end
-
-
-function old_play_chord(root, degree, seventh, minor, inversion, on_or_off)
-    root = root + params:get("transpose")
-    notes = musicutil.generate_chord_scale_degree(root, params:get("mode"), degree, seventh)
-
-    if minor then
-        -- check if major or minor
-        if notes[2] - notes[1] == 3 then
-            notes[2] = notes[2] + 1
-        else    
-            notes[2] = notes[2] - 1
-        end
-    end
-
-    -- if inversion is 1, then move the root note up one octave
-    if inversion == 0 then
-        notes[#notes+1] = notes[1] - 12
-    elseif inversion == 1 then
-        notes[#notes+1] = notes[1]- 12
-        notes[1] = notes[1] + 12
-    elseif inversion == 2 then
-        notes[#notes+1] = notes[1]- 12
-        notes[1] = notes[1] + 12
-        notes[2] = notes[2] + 12
-    end
-        
-    -- randomly shuffle the order of the notes
-    -- for i = 1, #notes do
-    --     j = math.random(i, #notes)
-    --     notes[i], notes[j] = notes[j], notes[i]
-    -- end
-
+function notes_to_engine(notes, on_or_off)
+    
     if on_or_off == 1 then
         clock.run(function()
             for i = 1, #notes do
-                skeys:on({name=instruments[params:get("mx_ins")],midi=notes[i],velocity = 70, release=0.5})
-
-                
-                -- if notes[i]-baseroot > 0 then
-                --     momentary[notes[i]-baseroot][2] = true
-                -- end
-
-                -- wait between 1 and 100 ms
+                local player = params:lookup_param("voice"):get_player()
+                player:note_on(notes[i], 1)
+                print(notes[i])
                 if params:get("humanize") > 0 then
                     clock.sleep(math.random(math.floor(params:get("humanize")/2),params:get("humanize"))/1000)
                 end
             end
-            -- grid_dirty = true
         end)
     else
         for i = 1, #notes do
-            skeys:off({name=instruments[params:get("mx_ins")],midi=notes[i]})
-
-            -- if notes[i]-baseroot > 0 then
-            --     momentary[notes[i]-baseroot][2] = false
-            -- end
-
+            local player = params:lookup_param("voice"):get_player()
+            player:note_off(notes[i])
         end
-        -- grid_dirty = true
     end
 
-    -- update the piano drawing
-    if on_or_off == 1 then
+    
 
-    else
+    -- if we just turned off all the notes, then clear the notes table
+    if on_or_off == 0 then
         notes = {}
-
     end
-
-
-    
-    -- trigger a screen update
-    chordname = transpose_string(params:get("transpose")+degree - 1)
-    
-    screen_dirty = true
 end
 
 
-
-function send_notes_to_engine(notes, on_or_off)
-    --
-end
 
 
 
@@ -390,5 +330,10 @@ function redraw() -------------- redraw() is automatically called by norns
     screen.update() -------------- update space
   end
 
-
-
+function panic()
+    print("panic")
+    for i = 0, 127 do
+        local player = params:lookup_param("voice"):get_player()
+        player:note_off(i)
+    end
+end
